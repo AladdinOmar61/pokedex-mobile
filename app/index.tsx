f0b875c04345bef4d2b9557288f6ecf0b3b271b0
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2025-08-25 22:31:27 -0400

    Created visible pokedex overviews for 150 pokemon
    - Created a backend function that grabs pokemon data with a limit of 150
    - styled the list with images, names and a chevron icon
    - made each pokemon a link/toucableOpacity that will soon route you to each individual pokemon by their id

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Touchable, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'expo-router'
+import { getPokemon, Pokemon } from '@/api/pokeapi'
+import { Ionicons } from '@expo/vector-icons';
 
 const Home = () => {
+
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  const retrievePokemon = async () => {
+    try {
+      const pokeResp = await getPokemon();
+      setPokemon(pokeResp);
+    } catch (err) {
+      console.error("Could not retrieve pokemon", err);
+    }
+  }
+
+  useEffect(() => {
+    retrievePokemon();
+  }, [])
+
   return (
-      <View>
-          <Link href="/(pokemon)/test">
-              <Text>Open test page</Text>
-          </Link>
-    </View>
+    <ScrollView>
+      {pokemon.map((p) => (
+        <Link href={`/(pokemon)/${p.id}`} asChild>
+          <TouchableOpacity>
+            <View style={styles.item}>
+              <Image source={{uri: p.image}} style={styles.preview} />
+              <Text style={styles.itemText}>{p.name}</Text>
+              <Ionicons name='chevron-forward' size={24} />
+            </View>
+          </TouchableOpacity>
+        </Link>
+      ))}
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  preview: {
+    width: 100,
+    height: 100
+  },
+  itemText: {
+    fontSize: 18,
+    textTransform: 'capitalize',
+    flex: 1
+  }
+})
 
 export default Home
