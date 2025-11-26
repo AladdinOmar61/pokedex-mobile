5f76f41afda4b1e0211ab404d49781856e001c40
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2025-11-26 00:21:07 -0500

    Began exporting assets from figma to replicate design
    Finished the design for the "all" and "gen1" buttons. Will circle back to "all" to make the pokemon pngs more crisp

--- app/index.tsx
+++ app/index.tsx
@@ -15,19 +15,45 @@ const Home = () => {
       <ScrollView style={{ marginBottom: insets.bottom }}>
         <Link href="/(pokemon)/all" asChild>
           <TouchableOpacity>
-            <View style={[styles.genGridItem, { width: width - 6 }]}>
+            <View style={[styles.genGridItem, { overflow: 'visible', alignItems: 'center' }]}>
               <ImageBackground
-                source={require("../assets/All-Gens.png")}
+                source={require("../assets/All-Gens-bg.png")}
                 style={styles.backgroundImageSize}
                 resizeMode='cover'
-              />
+              >
+                <View style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+                  <Image source={require("../assets/AllPokemon/Dragapult.png")} style={{ marginHorizontal: -5 }} />
+                  <Image source={require("../assets/AllPokemon/Talonflame.png")} style={{ marginHorizontal: -30, zIndex: 1, marginTop: 5 }} />
+                  <Image source={require("../assets/AllPokemon/Lucario.png")} style={{ marginHorizontal: -30, zIndex: 2, marginTop: 20 }} />
+                  <Image source={require("../assets/AllPokemon/Tyranitar.png")} style={{ marginHorizontal: -10, zIndex: 3, marginTop: 30 }} />
+                  <Image source={require("../assets/AllPokemon/Pikachu.png")} style={{ marginHorizontal: -40, zIndex: 4, marginTop: 50 }} />
+                  <Image source={require("../assets/AllPokemon/Gardevior.png")} style={{ marginHorizontal: -20, zIndex: 3, marginTop: 30 }} />
+                  <Image source={require("../assets/AllPokemon/Zoroark.png")} style={{ marginHorizontal: -20, zIndex: 2, marginTop: 20 }} />
+                  <Image source={require("../assets/AllPokemon/Mimikyu.png")} style={{ marginHorizontal: -30, zIndex: 1, marginTop: 20 }} />
+                  <Image source={require("../assets/AllPokemon/Ceruledge.png")} style={{ marginHorizontal: -10 }} />
+                </View>
+                <Text style={styles.allGensText}>ALL</Text>
+              </ImageBackground>
             </View>
           </TouchableOpacity>
         </Link>
         <View style={styles.genGrid}>
           <Link href={`(pokemon)/generations/${1}`} asChild>
             <TouchableOpacity>
-              <View style={[styles.genGridItem, { width: width / 2.1 }]}><Text>Gen 1</Text></View>
+              <View style={[styles.genGridItem, { width: width / 2.1 }]}>
+                <ImageBackground
+                  source={require("../assets/Gen1-bg.png")}
+                  style={styles.backgroundImageSize}
+                  resizeMode='cover'
+                >
+                  <View style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: 'center', alignItems: 'center', marginTop: "30%" }}>
+                    <Image source={require("../assets/Squirtle.png")} style={{ height: 39, width: 39 }} />
+                    <Image source={require("../assets/Bulbasaur.png")} style={{ height: 39, width: 39, marginTop: "16%" }} />
+                    <Image source={require("../assets/Charmander.png")} style={{ height: 39, width: 39 }} />
+                  </View>
+                  <Text style={styles.genText}>Gen 1</Text>
+                </ImageBackground>
+              </View>
             </TouchableOpacity>
           </Link>
           <Link href={`(pokemon)/generations/${2}`} asChild>
@@ -87,13 +113,33 @@ const styles = StyleSheet.create({
 
   genGridItem: {
     height: 150,
-    margin: 4
+    margin: 4,
+    borderBottomWidth: 1,
+    borderTopWidth: 1
   },
 
   backgroundImageSize: {
     flex: 1,
     width: "100%",
     height: "100%"
+  },
+
+  allGensText: {
+    position: 'absolute',
+    bottom: 0,
+    left: "45%",
+    color: "white",
+    fontFamily: "Silkscreen",
+    fontSize: 18
+  },
+
+  genText: {
+    position: 'absolute',
+    bottom: 0,
+    left: "38%",
+    color: "white",
+    fontFamily: "Silkscreen",
+    fontSize: 18
   }
 })
 
